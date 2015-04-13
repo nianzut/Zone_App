75bb92f2dee863b0cca53a355c16cde2dcf7cd5d
nianzut <dhfantang@gmail.com>
2015-04-13 01:12:58 -0400

    writing the LoginServer class openServer() closeServer()

--- app/src/main/java/mcalculator/com/tangnianzu/zoneapp/server/LoginServer.java
+++ app/src/main/java/mcalculator/com/tangnianzu/zoneapp/server/LoginServer.java
@@ -1,6 +1,10 @@
 package mcalculator.com.tangnianzu.zoneapp.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+
+import mcalculator.com.tangnianzu.zoneapp.tools.ServerConfig;
 
 /**
  * Created by tangnianzu on 4/12/15.
@@ -13,10 +17,46 @@ import java.net.Socket;
 public class LoginServer extends Thread {
 
 
-    private Socket socket=null;
+    private Socket socket = null;
 
-    public LoginServer(Socket socket){
-        this.socket=socket;
+    public LoginServer(Socket socket) {
+        this.socket = socket;
     }
 
+    //son thread: Client Request
+    public void run() {
+
+    }
+
+
+    private static ServerSocket server = null;
+    private static boolean b = true;
+
+
+    public static void openServer() throws Exception {
+        try {
+            server = new ServerSocket(Integer.parseInt(ServerConfig.getValue("server_port")));
+            while (b) {
+                new LoginServer(server.accept()).start();//give the client socket object to LoginServer and start the son thread
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            throw ex;
+        }
+    }
+
+    public static void closeServer() throws Exception {
+       try {
+           b = false;
+           server.close();
+       }catch (Exception ex){
+           throw ex;
+       }
+    }
 }
